35dd96a181e002b7b9632a30def8a3b315ded559
Yohan Belval <yohan.belval@gsoft.com>
2020-08-27 14:37:13 -0400

    [ISSUE-11] Removed manual setting of 'device_authorization_endpoint' since it was added ro the openid conf discovery call.

--- authentication/mso-issuer.ts
+++ authentication/mso-issuer.ts
@@ -6,14 +6,9 @@ class MsoIssuer<TClient extends Client> extends Issuer<TClient> {
     // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc#fetch-the-openid-connect-metadata-document
     static async discover(tenant = "common"): Promise<Issuer<Client>> {
         const issuer = await Issuer.discover(`https://login.microsoftonline.com/${tenant}/v2.0/.well-known/openid-configuration`);
-        
-        // Add the missing device code endpoint
-        Object.assign(issuer, {
-            device_authorization_endpoint: `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/devicecode`
-        })
 
         return issuer;
     }
-  }
+}
 
 export default MsoIssuer;
