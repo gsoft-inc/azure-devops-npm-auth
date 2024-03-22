489331ccfc1a596378dc1d9b80ca4bb0e54dedb6
pl-garcia <107400256+pl-garcia@users.noreply.github.com>
2024-03-22 07:05:25 +0100

    Feature/ado on premises (#3)
    * Added support for Azure DevOps Server
    
    * Modified cli.ts

--- cli.ts
+++ cli.ts
@@ -7,8 +7,9 @@ const args = require("minimist")(process.argv.slice(2), {
     cid: "client_id",
     tid: "tenant_id",
     ci: "continuous_integration_variable",
-    pbp: "project_base_path"
+    pbp: "project_base_path",
+    resid: "resource_id"
   }
 });
 
-(async () => await run(args.client_id, args.tenant_id, args.ci, args.pbp))();
+(async () => await run(args.client_id, args.tenant_id, args.ci, args.pbp, args.resid))();
